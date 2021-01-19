055f69f69f4c7aa3d42c5c3a3e327396d181e196
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-01-19 11:17:14 +0100

    commit 3 (master) Method Scanner (there is an error, the program does not work)

--- src/sk/kosickaakademia/illiaspivak/regex/Main.java
+++ src/sk/kosickaakademia/illiaspivak/regex/Main.java
@@ -1,8 +1,13 @@
 package sk.kosickaakademia.illiaspivak.regex;
 
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Enter number:");
+        Scanner scanner = new Scanner(System.in);
+        String number = reader.readLine();
+        System.out.println(number);
     }
 }
