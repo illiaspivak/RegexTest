6ac55041e7ee2c9892df24c7dcb91ade8d3f6f6b
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-01-19 11:31:15 +0100

    commit 6 (SPZ) merged the hotfix branch with the master branch; continue working in the SPZ branch

--- src/sk/kosickaakademia/illiaspivak/regex/NoRegex.java
+++ src/sk/kosickaakademia/illiaspivak/regex/NoRegex.java
@@ -7,6 +7,14 @@ public class NoRegex {
         text = text.toUpperCase().trim();
         if (text.length() != 7)
             return false;
+        for(int i=0; i<=1; i++){ //kontrola prvych dvuch znakov, ci su to pismena
+            if(!Character.isLetter(text.charAt(i)))
+                return false;
+        }
+        for(int i=2; i<=6; i++){ //kontrola dalsich znakov
+            if(!Character.isLetterOrDigit(text.charAt(i)))
+                return false;
+        }
         return true;
     }
 }
