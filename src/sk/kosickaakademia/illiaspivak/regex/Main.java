a9cbbcc0a81955481e91cb8391ed5e383e7fd33c
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-01-19 11:25:46 +0100

    commit 5 (hotfix) switched to the master branch; created a new branch to fix; fixed Main

--- src/sk/kosickaakademia/illiaspivak/regex/Main.java
+++ src/sk/kosickaakademia/illiaspivak/regex/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Enter number:");
         Scanner scanner = new Scanner(System.in);
-        String number = reader.readLine();
+        String number = scanner.nextLine();
         System.out.println(number);
     }
 }
