481bc0ad3878b1439156920a2bbbf7ee6bf492be
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-01-19 11:40:12 +0100

    commit 7 (master) deleted everything from Main

--- src/sk/kosickaakademia/illiaspivak/regex/Main.java
+++ src/sk/kosickaakademia/illiaspivak/regex/Main.java
@@ -5,9 +5,5 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter number:");
-        Scanner scanner = new Scanner(System.in);
-        String number = scanner.nextLine();
-        System.out.println(number);
     }
 }
